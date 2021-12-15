04fc8a5298368b30dc486a36c0441cf41b6abb64
Jay Joo <jj534@cornell.edu>
2021-12-15 11:55:11 -0500

    Init repo

---
+++ init.js
@@ -0,0 +1,10 @@
+(function () {
+  // RHS table: edit card titles
+  document.getElementById(
+    "win0divDERIVED_SSS_SCL_SSS_SC_LNK2_NAMEGP"
+  ).innerHTML = "Enrollment";
+
+  document.getElementById(
+    "win0divDERIVED_SSS_SCL_SSS_SC_LNK1_NAMEGP"
+  ).innerHTML = "Resources";
+})();
